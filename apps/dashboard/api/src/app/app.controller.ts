cf63b881533cfe2da7eed874ff6972d9846a64b8
ESnark <settimeout1000@gmail.com>
2022-02-06 00:08:59 +0900

    Dashboard TCP response

--- apps/dashboard/api/src/app/app.controller.ts
+++ apps/dashboard/api/src/app/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
 
 import { DashboardApiAppService } from '@vnm/domain';
+import { Observable, from } from 'rxjs';
 
 @Controller()
 export class AppController {
@@ -9,5 +11,12 @@ export class AppController {
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @MessagePattern({ cmd: 'dashboard-sum' })
+  accumulate(data: number[]): Observable<number> {
+    console.log('calling sum from dashboard');
+    const sum = data[0] + data[1] + data[2];
+    return from([sum]);
   }
 }
