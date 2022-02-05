8f670f88be8ecf74267e1d11b084893a2d33a800
ESnark <settimeout1000@gmail.com>
2022-02-05 23:31:33 +0900

    ServeStaticModule installed

--- apps/dashboard/api/src/app/app.module.ts
+++ apps/dashboard/api/src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 import { AppController } from './app.controller';
 import { DashboardApiAppService } from '@vnm/domain';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, 'public'),
+      exclude: ['/api*', '/dashboard/api*']
+    })
+  ],
   controllers: [AppController],
   providers: [DashboardApiAppService],
 })
