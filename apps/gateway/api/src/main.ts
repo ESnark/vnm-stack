a48c8edcd40048b2e3589f7512662b29617dcab2
ESnark <settimeout1000@gmail.com>
2022-02-05 20:43:45 +0900

    no message

--- apps/gateway/api/src/main.ts
+++ apps/gateway/api/src/main.ts
@@ -6,16 +6,19 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
+import { loadGatewayConfiguration } from '@vnm/shared';
+
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+
+  const config = loadGatewayConfiguration();
+
+  const port = config.HTTP_PORT || process.env.PORT || 8000;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://localhost:${port}`
   );
 }
 
