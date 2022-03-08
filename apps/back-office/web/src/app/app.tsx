8d521e368e56861a79395972a6e7afb9fd8ad5c0
ESnark <settimeout1000@gmail.com>
2022-03-09 01:22:35 +0900

    Replace vue packages into react packages

---
+++ apps/back-office/web/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="back-office-web" />
+      <div />
+    </>
+  );
+}
+
+export default App;
